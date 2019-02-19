83724e1dd2d9286da8b25965dc37f0b5c3769328
Justin Yu <35335269+JustinYuu@users.noreply.github.com>
2019-02-19 15:01:49 +0800

    Update 2019-02-19-PAT-A 1022.cpp

--- _posts/2019-02-19-PAT-A 1022.cpp
+++ _posts/2019-02-19-PAT-A 1022.cpp
@@ -93,7 +93,7 @@ In this problem,we must use citation type to cite map as a parameter of funtion 
 Here are the codes:
   
     {% highlight ruby %}
-    #include <iostream>
+	#include <iostream>
 	#include <cstdio>
 	#include <map>
 	#include <set>
@@ -104,59 +104,59 @@ Here are the codes:
 
 	void query(map< string,set<int> >& mp, string& str)
 	{
-		if(mp.find(str)==mp.end())
-			printf("Not Found\n");
-		else
-		{
-			for(set<int>::iterator it=mp[str].begin();it!=mp[str].end();it++)
-				printf("%07d\n",*it);
-		}
+	    if(mp.find(str)==mp.end())
+		printf("Not Found\n");
+	    else
+	    {
+		for(set<int>::iterator it=mp[str].begin();it!=mp[str].end();it++)
+		    printf("%07d\n",*it);
+	    }
 	}
 
 	int main()
 	{
-		int n,m,id,type;
-		string title,author,key,pub,year;
-		scanf("%d",&n);
-		for(int i=0;i<n;++i)
+	    int n,m,id,type;
+	    string title,author,key,pub,year;
+	    scanf("%d",&n);
+	    for(int i=0;i<n;++i)
+	    {
+		scanf("%d",&id);
+		char c = getchar();
+		getline(cin,title);
+		mpTitle[title].insert(id);
+		getline(cin,author);
+		mpAuthor[author].insert(id);
+		while(cin>>key)
 		{
-			scanf("%d",&id);
-			char c = getchar();
-			getline(cin,title);
-			mpTitle[title].insert(id);
-			getline(cin,author);
-			mpAuthor[author].insert(id);
-			while(cin>>key)
-			{
-				mpKey[key].insert(id);
-				c = getchar();
-				if(c=='\n')
-					break;
-			}
-			getline(cin,pub);
-			mpPub[pub].insert(id);
-			getline(cin,year);
-			mpYear[year].insert(id);
+		    mpKey[key].insert(id);
+		    c = getchar();
+		    if(c=='\n')
+			break;
 		}
-		string temp;
-		scanf("%d",&m);
-		for(int i=0;i<m;++i)
-		{
-			scanf("%d: ",&type);
-			getline(cin,temp);
-			cout<<type<<": "<<temp<<endl;
-			if(type==1)
-				query(mpTitle,temp);
-			else if(type==2)
-				query(mpAuthor,temp);
-			else if(type==3)
-				query(mpAuthor,temp);
-			else if(type==4)
-				query(mpAuthor,temp);
-			else
-				query(mpYear,temp);
-		}
-		return 0;
+		getline(cin,pub);
+		mpPub[pub].insert(id);
+		getline(cin,year);
+		mpYear[year].insert(id);
+	    }
+	    string temp;
+	    scanf("%d",&m);
+	    for(int i=0;i<m;++i)
+	    {
+		scanf("%d: ",&type);
+		getline(cin,temp);
+		cout<<type<<": "<<temp<<endl;
+		if(type==1)
+		    query(mpTitle,temp);
+		else if(type==2)
+		    query(mpAuthor,temp);
+		else if(type==3)
+		    query(mpKey,temp);
+		else if(type==4)
+		    query(mpPub,temp);
+		else
+		    query(mpYear,temp);
+	    }
+	    return 0;
 	}
 	{% endhighlight %}
 ---	
